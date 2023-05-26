691f08530caf9a7325905bb70f3fc4b083fee648
junhaolics <junhaolics@foxmail.com>
2023-05-26 15:21:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# alex_for_4GDTUtoNetty
+springboot基于netty与4G DTU物联网设备通信，接收和下发数据和指令，进行远程控制，该项目与vue app是配套的，vue demo项目可打包进行网页pc部署，也可打包成app，网页部署最好用nginx进行反向代理，相关的资料网上很好找
---
+++ src/main/java/com/rxkj/util/AlexSatic.java
@@ -0,0 +1,15 @@
+package com.rxkj.util;
+
+public class AlexSatic {
+
+    public static final String PROTOCOL_HEAD = "alex";
+    //public static final String PROTOCOL_HEAD = "AAAA";
+
+
+    public static final String HEARTBEAT_CONTENT = "alex01";
+    //public static final String HEARTBEAT_CONTENT = "alex08";
+    public static final String DATA_HEAD = "alex01-data:";
+    //public static final String DATA_HEAD = "AAAA02";
+
+
+}
