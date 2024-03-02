d7d181b89dbc379283cc2d6dc59195f00b53c37d
lilimu <junhaolics@foxmail.com>
2024-03-02 15:20:23 +0800

    优化dtu状态获取逻辑

--- src/main/java/com/rxkj/server/handler/PlcStatusHandler.java
+++ src/main/java/com/rxkj/server/handler/PlcStatusHandler.java
@@ -4,7 +4,9 @@ import com.rxkj.enums.CommandEnum;
 import com.rxkj.enums.CommandLengthEnum;
 import com.rxkj.enums.KeywordEnum;
 import com.rxkj.enums.TimeoutEnum;
+import com.rxkj.mapper.DeviceList;
 import com.rxkj.message.MessageA;
+import com.rxkj.message.SseMessage;
 import com.rxkj.message.StatusMessage;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -21,8 +23,28 @@ public class PlcStatusHandler extends SimpleChannelInboundHandler<StatusMessage>
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, StatusMessage statusMessage) throws Exception {
         //#todo:plc状态信息，转发给前端
-        //#todo:服务器回复指令0xF
+        //#todo:服务器回复指令0xFE
         log.info("statusMessage "+statusMessage);
+        SseMessage message = new SseMessage();
+        if((message= (SseMessage) DeviceList.getDeviceVector().get(Integer.parseInt(statusMessage.getDeviceId())))!=null){
+            String coilAddress=statusMessage.getAuxiliaryCoils();
+            String outputCoil=statusMessage.getOutputCoil();
+            if(coilAddress.equals("040C")){
+                message.setInletValve(1);
+            } else if (coilAddress.equals("040A")) {
+                message.setVentingValve(1);
+            } else if (coilAddress.equals("0406")) {
+                message.setSampleValve(1);
+            }else if(coilAddress.equals("040D")){
+                message.setInletValve(0);
+            } else if (coilAddress.equals("040B")) {
+                message.setVentingValve(0);
+            }else if (coilAddress.equals("0407")) {
+                message.setSampleValve(0);
+            }
+            DeviceList.add(Integer.parseInt(statusMessage.getDeviceId()),message);
+            log.info("StatusMessage processing"+DeviceList.getDeviceVector().size());
+        }
         //回复客服端
         // log.info("CommandEnum "+CommandEnum.UPLOAD_COMMAND.value);
         //#todo:更新checksum
