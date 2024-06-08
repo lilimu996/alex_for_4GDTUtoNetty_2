d1203b5c437e1ee1da510df05814ecb4d76c1b84
hsxazj <2279538834@qq.com>
2024-06-08 15:59:18 +0800

    修复权限校验异常的bug，添加权限日志打印

--- src/main/java/com/rxkj/controller/UserController.java
+++ src/main/java/com/rxkj/controller/UserController.java
@@ -19,97 +19,84 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    @Resource
-    UserServiceImpl userService;
+	@Resource
+	UserServiceImpl userService;
 
-    @GetMapping("/")
-    public String helloUser() {
-        return "users!";
-    }
+	@GetMapping("/")
+	public String helloUser() {
+		return "users!";
+	}
 
-    @GetMapping("/login")
-    public R login(@ModelAttribute LoginDto loginDto) {
-        return userService.login(loginDto.getUserName(), loginDto.getPassword());
-    }
+	@GetMapping("/login")
+	public R login(@ModelAttribute LoginDto loginDto) {
+		return userService.login(loginDto.getUserName(), loginDto.getPassword());
+	}
 
-    @GetMapping("/logout")
-    public R<String> logout(@AuthenticationPrincipal MeiFenUser meiFenUser) {
-        String userNumbers = meiFenUser.getUser().getUserNumbers();
-        return userService.logout(userNumbers);
-    }
+	@GetMapping("/logout")
+	public R<String> logout(@AuthenticationPrincipal MeiFenUser meiFenUser) {
+		String userNumbers = meiFenUser.getUser().getUserNumbers();
+		return userService.logout(userNumbers);
+	}
 
 
-    @PreAuthorize("hasAuthority('1')")
-    @GetMapping("/selectAll")
-    public R<List<Users>> selectAll() {
-        List<Users> userList = userService.list(null);
-        return R.success(userList);
-    }
+	@PreAuthorize("hasAuthority('1')")
+	@GetMapping("/selectAll")
+	public R<List<Users>> selectAll() {
+		List<Users> userList = userService.list(null);
+		return R.success(userList);
+	}
 
-    @PreAuthorize("hasAuthority('1')")
-    @GetMapping("/selectPage")
-    public R<IPage<Users>> selectPage(Integer current, Integer size) {
-        IPage<Users> page = new Page<>(current, size);
-        userService.page(page);
-        return R.success(page);
-    }
+	@PreAuthorize("hasAuthority('1')")
+	@GetMapping("/selectPage")
+	public R<IPage<Users>> selectPage(Integer current, Integer size) {
+		IPage<Users> page = new Page<>(current, size);
+		userService.page(page);
+		return R.success(page);
+	}
 
-    @PreAuthorize("hasAuthority('1')")
-    @PostMapping("/addUser")
-    public R addUser(@RequestBody Users user) {
-        if (!checkStringNumbers(user.getUserNumbers(), 18)) {
-            return R.error("Incorrectly formatted User ID number !!");
-        }
-        return userService.addUser(user);
-    }
+	@PreAuthorize("hasAuthority('1')")
+	@PostMapping("/addUser")
+	public R addUser(@RequestBody Users user) {
+		if (!checkStringNumbers(user.getUserNumbers(), 18)) {
+			return R.error("Incorrectly formatted User ID number !!");
+		}
+		return userService.addUser(user);
+	}
 
-    /*
-    @PostMapping("/updateUserById")
-    public R<String> updateUserById(@RequestBody Users user) {
-        if (!checkStringNumbers(user.getUserNumbers(), 18)) {
-            return R.error("Incorrectly formatted User ID number !!");
-        }
-        if (userService.updateById(user)) {
-            return R.success("update success!");
-        }
-        return R.error("uptate fails!!");
-    }
-    */
+	@PreAuthorize("hasAuthority('1')")
+	@PostMapping("/deleteByName")
+	public R<String> deleteUserByName(@RequestBody Users user) {
+		QueryWrapper<Users> qw = new QueryWrapper<>();
+		qw.eq("user_name", user.getUserName());
+		if (userService.remove(qw)) {
+			return R.success("delete success!!");
+		}
+		return R.error("delete fails!!");
+	}
 
-    @PreAuthorize("hasAuthority('1')")
-    @PostMapping("/deleteByName")
-    public R<String> deleteUserByName(@RequestBody Users user) {
-        QueryWrapper<Users> qw = new QueryWrapper<>();
-        qw.eq("user_name", user.getUserName());
-        if (userService.remove(qw)) {
-            return R.success("delete success!!");
-        }
-        return R.error("delete fails!!");
-    }
+	@PreAuthorize("hasAuthority('1')")
+	@PostMapping("/deleteById")
+	public R<String> deleteUserById(@RequestBody Users user) {
+		QueryWrapper<Users> qw = new QueryWrapper<>();
+		qw.eq("user_numbers", user.getUserNumbers());
+		if (userService.remove(qw)) {
+			return R.success("delete success!!");
+		}
+		return R.error("delete fails!!");
+	}
 
-    @PreAuthorize("hasAuthority('1')")
-    @PostMapping("/deleteById")
-    public R<String> deleteUserById(@RequestBody Users user) {
-        QueryWrapper<Users> qw = new QueryWrapper<>();
-        qw.eq("user_numbers", user.getUserNumbers());
-        if (userService.remove(qw)) {
-            return R.success("delete success!!");
-        }
-        return R.error("delete fails!!");
-    }
-
-    /**
-     * 校验字符串中的数字格式
-     * size:数字的个数
-     */
-    private static boolean checkStringNumbers(String numberString, int size) {
-        // 校验身份证格式
-        if (numberString.length() == size) {
-            if (numberString.substring(0, size - 1).matches("\\d+")) {
-                return true;
-            }
-        }
-        return false;
-    }
+	/**
+	 * 校验字符串中的数字格式
+	 * size:数字的个数
+	 */
+	private static boolean checkStringNumbers(String numberString, int size) {
+		// 校验身份证格式
+		if (numberString.length() == size) {
+			if (numberString.substring(0, size - 1).matches("\\d+")) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
