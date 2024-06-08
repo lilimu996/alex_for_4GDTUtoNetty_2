6258d0299b09a8631e98c5f2c84aa13ac8d16864
hsxazj <2279538834@qq.com>
2024-06-08 16:43:48 +0800

    修改跨域配置和springboot版本

--- src/main/java/com/rxkj/config/CorsConfig.java
+++ src/main/java/com/rxkj/config/CorsConfig.java
@@ -8,19 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 设置允许跨域的路径
-        registry.addMapping("/**")
-                // 设置允许跨域请求的域名
-
-                // 跨域配置  若不生效，切换spring-webmvc版本即可
-                .allowedOrigins("*")
-                // 设置允许的请求方式
-                .allowedMethods("GET", "POST", "DELETE", "PUT")
-                // 设置允许的header属性
-                .allowedHeaders("*")
-                // 跨域允许时间
-                .maxAge(3600);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// 设置允许跨域的路径
+		registry.addMapping("/**")
+				// 跨域配置  若不生效，切换spring-webmvc版本即可
+				.allowedOriginPatterns("*")
+				// 设置允许的请求方式
+				.allowedMethods("GET", "POST", "DELETE", "PUT")
+				// 设置允许的header属性
+				.allowedHeaders("*")
+				// 跨域允许时间
+				.maxAge(3600);
+	}
 }
